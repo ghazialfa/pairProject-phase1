f403e9718cc3cc591c3fb9ed3d6a18248591d27e
ghazialfa <ghazialfawwaz01@gmail.com>
2024-02-22 17:22:50 +0700

    correct models dll

--- models/user.js
+++ models/user.js
@@ -17,7 +17,11 @@ module.exports = (sequelize, DataTypes) => {
     {
       userName: {
         type: DataTypes.STRING,
+<<<<<<< HEAD
         unique: true
+=======
+        unique: true,
+>>>>>>> c589a25 (correct models dll)
       },
       email: {
         type: DataTypes.STRING,
@@ -29,13 +33,18 @@ module.exports = (sequelize, DataTypes) => {
       },
       role: {
         type: DataTypes.STRING,
+<<<<<<< HEAD
         defaultValue: 'user',
+=======
+        defaultValue: "user",
+>>>>>>> c589a25 (correct models dll)
         allowNull: false,
       },
     },
     {
       sequelize,
       modelName: "User",
+<<<<<<< HEAD
       hooks: {beforeCreate: (instance, options) => {
         const bcrypt = require('bcryptjs');
         const salt = bcrypt.genSaltSync(10);
@@ -44,6 +53,17 @@ module.exports = (sequelize, DataTypes) => {
         instance.password = hash
         }
       }
+=======
+      hooks: {
+        beforeCreate: (instance, options) => {
+          const bcrypt = require("bcryptjs");
+          const salt = bcrypt.genSaltSync(10);
+          const hash = bcrypt.hashSync(instance.password, salt);
+          console.log(hash);
+          instance.password = hash;
+        },
+      },
+>>>>>>> c589a25 (correct models dll)
     }
   );
   return User;
