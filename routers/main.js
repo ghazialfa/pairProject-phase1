8b070eac0a5f25153c0ad76176d7e18776291b70
ghazialfa <ghazialfawwaz01@gmail.com>
2024-02-22 13:01:07 +0700

    view 0.1

--- routers/main.js
+++ routers/main.js
@@ -1,1 +1,20 @@
 "use strict";
+const express = require("express");
+const Controller = require("../controllers/controller");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.redirect("/login");
+});
+
+router.use("/login");
+router.use("/posts");
+// router.use("/profile");
+router.use("/user/:userId");
+router.use("/users/:userId/profile/:profileId");
+router.use("/users/:userId/profile/add");
+router.post("/users/:userId/profile/add");
+router.use("/users/:userId/profile/:profileId/edit");
+router.post("/users/:userId/profile/:profileId/edit");
+
+module.exports = router;
