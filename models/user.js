df65206bfb7297c3b441b7b5798a9558fde27771
billy.massie <billybois68@gmail.com>
2024-02-22 18:02:01 +0700

    not empty validation

--- models/user.js
+++ models/user.js
@@ -16,6 +16,7 @@ module.exports = (sequelize, DataTypes) => {
   User.init(
     {
       userName: {
+        allowNull: false,
         type: DataTypes.STRING,
         unique: true,
         validate: {
